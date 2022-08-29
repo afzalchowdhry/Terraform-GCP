da1e62a0e69b5b7c911b3c2a88e3ddbca62aabad
afzalchowdhry <36516080+afzalchowdhry@users.noreply.github.com>
2022-08-29 21:38:55 +0530

    Create main.tf

---
+++ CloudStorage/main.tf
@@ -0,0 +1,25 @@
+resource "google_storage_bucket" "GCS1" {
+    name = "bucket-using-tfscript-afzal"
+    location = "US"  
+    storage_class = "NEARLINE"
+    labels = {
+      "env" = "test"
+      "dept" = "it"
+    }
+
+    lifecycle_rule {
+      condition {
+        age = 1
+      }
+      action {
+        type = "Delete"
+      }
+    }
+}
+
+resource "google_storage_bucket_object" "object" {
+    name = "Time_Tracking"
+    source = "C:/Users/002VH4744/Documents/Time_Tracking.png"
+    bucket = google_storage_bucket.GCS1.name
+  
+}
